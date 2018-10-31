de5edcfc3d32cf0d3b0cf4d18a44b1c5cf48dd8c
Phan Dang Duy Thinh <phandangduythinh@gmail.com>
2018-10-31 19:01:57 +0800

    Update to support MAC OS

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+xargsOPT=""
+
+OS=`uname -s`
+case "OS" in
+    Linux*)
+        xargsOPT="-r"
+    ;;
+esac
+
 # remove exited containers:
-docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+docker ps --filter status=dead --filter status=exited -aq | xargs $xargsOPT docker rm -v
     
 # remove unused images:
-docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs $xargsOPT docker rmi
 
 # remove unused volumes:
 find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
   docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
-) | xargs -r rm -fr
+) | xargs $xargsOPT rm -fr
